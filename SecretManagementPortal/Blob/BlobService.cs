853aa005a95a01c8ff138d3022b7eb2d29a5db57
James Peng <chpeng16@winbond.com>
2024-06-14 18:14:40 +0800

    .

---
+++ SecretManagementPortal/Blob/BlobService.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Storage.Blobs;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SecretManagementPortal.Blob
+{
+    public class BlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string _containerName;
+
+        public BlobService(string connectionString, string containerName)
+        {
+            _blobServiceClient = new BlobServiceClient(connectionString);
+            _containerName = containerName;
+        }
+
+        public async Task UploadFileAsync(Stream fileStream, string fileName)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            var blobClient = containerClient.GetBlobClient(fileName);
+            await blobClient.UploadAsync(fileStream, overwrite: true);
+        }
+    }
+}
--- SecretManagementPortal/Program.cs
+++ SecretManagementPortal/Program.cs
@@ -1,4 +1,23 @@
+using SecretManagementPortal.Blob;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// 從環境變數中獲取連線字串，如果環境變數不存在則回退到配置文件
+var connectionString = Environment.GetEnvironmentVariable("StorageConnectionString") ?? builder.Configuration["AzureStorageConfig:ConnectionString"];
+var containerName = builder.Configuration["AzureStorageConfig:ContainerName"];
+
+var blobService = new BlobService(
+            connectionString,
+            containerName
+          );    
+
+builder.Services.AddScoped<BlobService>(serviceProvider => blobService);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MaxRequestBodySize = 52428800; // 例如，這裡設置為50MB
+});
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -22,4 +41,7 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+app.MapControllers(); // 確保控制器路由被註冊
+
 app.Run();
+
